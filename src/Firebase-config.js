415e36592969167817b1f7eb97c7deb85f5bcfec
info-coe <coeteam@infomericainc.in>
2024-09-16 18:07:33 +0530

    updated firebase config

--- src/Firebase-config.js
+++ src/Firebase-config.js
@@ -8,11 +8,6 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_PROJECTID,
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   appId: process.env.REACT_APP_APPID
-
-  // apiKey: "AIzaSyCfudGcbJqupAg_ddFFj-LgV8KfAMap0BA",
-  // projectId: "insightify-infomericainc",
-  // storageBucket: "insightify-infomericainc.appspot.com",
-  // appId: "1:476029687149:android:dd470584be4c4b44753664"
 };
 
 // Initialize Firebase
